7cff3651cb4b8b595e4aa55e6095edd269d3dc3b
Vichooliva <60617258+Vichooliva@users.noreply.github.com>
2020-07-29 18:00:27 -0400

    Add files via upload

--- voz2.py
+++ voz2.py
@@ -1,96 +1,48 @@
-from imutils.video import VideoStream
-import numpy as np
-import argparse
-import imutils
-import time
-import cv2
-import pyttsx3
-import os
-import threading
-import xlsxwriter
-import csv
-from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
-from datetime import datetime
-from PyQt5 import QtGui, QtTest, QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTimeEdit
-from PyQt5.QtCore import QTime
-import sys
-from openpyxl import load_workbook
-from PyQt5.QtWidgets import * 
-from PyQt5.QtGui import * 
-from PyQt5.QtCore import Qt 
+from pygame import mixer
+from tkinter import *
+from tkinter.font import Font
+from PIL import  Image
+import tkinter as tk
 
- 
+def voz(mascarillaBool):
+    if mascarillaBool==1:
+        mixer.init()
+        mixer.music.load('Audios/Masc/Francisco1.mp3')
+        mixer.music.play()
+    else:
+        mixer.init()
+        mixer.music.load('Audios/Masc/Francisco2.mp3')
+        mixer.music.play()
 
-def paass(label):
+def MostrarUI(T,M):
+    foto = "ui-pasaste.png" if M ==1 else "ui-denegado.png"
+    root = Tk()
+    root.overrideredirect(True)
+    fontStyle = Font(family="Arial", size=48)
 
-        class Window(QWidget):
-            def __init__(self):
-                super().__init__()
-                self.setStyleSheet("background-color: black;")
-                self.setWindowTitle("no title") 
-                self.showMaximized()
-                self.MyTime()
+    #se calcula el posicionamiento de la ventana emergente, esto podría no funcionar el raspberry, en caso negativo comentar esto y la linea 38
+    windowWidth = root.winfo_reqwidth()
+    windowHeight = root.winfo_reqheight()
+    positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(root.winfo_screenheight()/3 - windowHeight/2)
 
-            def MyTime(self):
-                vbox = QVBoxLayout()
-                label3 = QLabel(self)
-                vbox.addWidget(label3)
-                filepath1="Datos.xlsx"
-        # load demo.xlsx 
-                wb1=load_workbook(filepath1)
-# select demo.xlsx
-                sheet1=wb1.active
-# set value for cell A1=1
-                engine = pyttsx3.init()
-                if (sheet1['A1'].value==0):
-                    label = QLabel()
-                    label.setText("Ponte mascarilla")
-                    label.setFont(QtGui.QFont("Sanserif", 50))
-                    label.setAlignment(QtCore.Qt.AlignCenter)
-                    label.setStyleSheet("background-color:black; color:red;")
-                    engine.setProperty('rate',140)
-                    engine.say("ponte mascarilla")
-                    vbox.addWidget(label)
-                # Se reproduce la voz
-                    engine.runAndWait()
-                if (sheet1['A1'].value==1):
-                    label = QLabel()
-                    label.setText("Mascarilla puesta correctamente")
-                    label.setFont(QtGui.QFont("Sanserif", 50))
-                    label.setAlignment(QtCore.Qt.AlignCenter)
-                    label.setStyleSheet("background-color:black; color:lightgreen;")
-                    engine.setProperty('rate',140)
-                    engine.say("siga adelante")
-                    vbox.addWidget(label)
-                # Se reproduce la voz
-                    engine.runAndWait()
-                label2 = QLabel()
-                label2.setText("T°:")
-                label2.setFont(QtGui.QFont("Sanserif", 50))
-                label2.setStyleSheet("background-color:black;color:white;")
-                pixmap = QPixmap('LOGO-EQYS.png')
-                label3.setPixmap(pixmap)
-                label3.setStyleSheet("background-color:black;")
-                vbox.addWidget(label2)
-                label3.setAlignment(QtCore.Qt.AlignCenter)
-                self.setLayout(vbox)
+    #Poner una imagen y texto encima con el atributo "compound", los colores se asignan con la funcion from_rgb, que toma una tupla (r,g,b) como parámetro
+    #el fondo de la ventana tendrá el mismo color que el marco de la imagen (rgb 155,159,162)
+    root.image=tk.PhotoImage(file=foto)
+    image=Label(root,image=root.image,text="\n\n\n\n\n\n"+str(T)+"°C",bg=from_rgb((155,159,162)),compound=CENTER,font=fontStyle,fg=from_rgb((93,180,39)) if M==1 else from_rgb((254,0,0)))
 
-                
-                QtTest.QTest.qWait(2000)
-         
+    #a esta ventana emergente se le transparenta el color rgb 155 159 162 para hacer la ilusión de que es una imagen flotando.
+    root.attributes("-transparentcolor",from_rgb((155,159,162)) )
+    #autodestrucción de la ui en 3 segundos
+    root.after(3000,lambda:root.destroy())
+    #poner las coordenadas de posicion en la ventana
+    root.geometry("+{}+{}".format(positionRight-60, positionDown))
 
-               
-         
-                
-        App = QApplication(sys.argv)
-        dialog = QDialog()
-        dialog.showFullScreen()
-        window = Window()
-        #sys.exit(App.exec())
- 
- 
-#if __name__ == "__main__":
-# textTovoice(sys.argv[0]) 
+    image.pack()
+    root.mainloop()
+
+def from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return "#%02x%02x%02x" % rgb
+
