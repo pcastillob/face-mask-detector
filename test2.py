3b666f43918428282201d7247c488ee397e6c39a
pcastillob <37872029+pcastillob@users.noreply.github.com>
2020-08-18 00:40:31 -0400

    Fondo negro con marco blanco

--- test2.py
+++ test2.py
@@ -11,12 +11,11 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from queue import Full, Queue, Empty
 from threading import Thread, Event
-from MYPIL import ImageTk, Image
+#from MYPIL import ImageTk, Image
 import cv2
 import voz2
 import os
 import sys
-
 
 logger = logging.getLogger("VideoStream")
 
@@ -52,7 +51,7 @@ def cargar_modelo():
     maskNet = load_model(args["model"])
     return faceNet,maskNet
 
-def video_stream_loop(video_stream: cv2.VideoCapture, img_ui: Queue,img_process: Queue, stop_event: Event,pausa_event: Event):
+def video_stream_loop(video_stream: cv2.VideoCapture, img_process: Queue, stop_event: Event,pausa_event: Event):
     print("VIDEO STREAM LOOP")
     while not stop_event.is_set():
         try:
@@ -60,10 +59,9 @@ def video_stream_loop(video_stream: cv2.VideoCapture, img_ui: Queue,img_process:
             if success:
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 if not pausa_event.is_set():
-                    img_ui.put(img, timeout=15)
-                    img_process.put(img,timeout=15)
+                    img_process.put(img,timeout=30)
                 else:
-                    img_ui.put(img, timeout=30)
+                    time.sleep(0.3)
         except Full:
             pass  # try again with a newer frame
     print("STOP DEL VIDEO")
@@ -110,7 +108,7 @@ def detect_and_predict_mask(frame,faceNet,maskNet):
         preds=maskNet.predict(faces,batch_size=32)
     return (locs,preds)
 
-def processing_loop(input_queue: Queue,  stop_event: Event,pausa_event: Event, faceNet , maskNet):
+def processing_loop(input_queue: Queue,resultado_queue: Queue,  stop_event: Event,pausa_event: Event, faceNet , maskNet):
     print("PROCESSING")
     contador = 0
     totalMask= 0
@@ -139,29 +137,27 @@ def processing_loop(input_queue: Queue,  stop_event: Event,pausa_event: Event, f
                 totalMask +=  mask
                 totalSinMask +=  withoutMask
                 contador = contador+1
-                
                 if	contador == 15:
                     contador = 0
                     print("Entró al if")
                     print("Total sin mask: " + str(totalSinMask))
                     print("Total con mask: " + str(totalMask))
                     pausa_event.set()
+                    #modificar acá para poner el codigo del sensor 
                     if totalMask > totalSinMask:
+                        #TIENE MASCARILLA
                         print("resultado: con mascarilla")
+                        resultado_queue.put_nowait("pasa")
                         voz2.voz(1)
                         time.sleep(4)
-                        pausa_event.clear()
-                       # with output_queue.mutex:
-                        #    output_queue.queue.clear()
-                     #   voz2.MostrarUI(37,1)	
+                        pausa_event.clear()	
                     else:
+                        #NO TIENE MASCARILLA
                         print("resultado: sin mascarilla")
+                        resultado_queue.put_nowait("denegado")
                         voz2.voz(0)
                         time.sleep(4)
                         pausa_event.clear()
-                        #with output_queue.mutex:
-                         #   output_queue.queue.clear()
-                      #  voz2.MostrarUI(38.5,0)
             # We need a timeout here to not get stuck when no images are retrieved from the queue
            # output_queue.put(img, timeout=33)
         except Full:
@@ -170,31 +166,30 @@ def processing_loop(input_queue: Queue,  stop_event: Event,pausa_event: Event, f
     sys.exit()
 
 class App:
-    def __init__(self, window, window_title, image_queue: Queue, image_dimensions: tuple):
+    def __init__(self, window, resultado_queue:Queue):
+        print("iniciando UI")
         self.window = window
-        self.window.title(window_title)
-
-        self.image_queue = image_queue
+        #self.window.overrideredirect(True)
+        #self.window.wm_attributes("-fullscreen","true")
+        self.resultado_queue = resultado_queue
         self.fontStyle = Font(family="Arial", size=18)
         # Create a canvas that can fit the above video source size
-        self.imagen=PhotoImage(file="LOGO-EQYS.png")
-        self.labelArriba = tkinter.Label(window,bg="black",image=self.imagen).pack()
-        self.canvas = tkinter.Canvas(window, width=image_dimensions[0], height=image_dimensions[1])
-        self.canvas.pack()
-        self.labelAbajo = tkinter.Label(window,text="Por favor, acérquese al tótem.",font=self.fontStyle,pady=20).pack()
-        
+        self.imagen=PhotoImage(file="negro-blank.png")
+        self.label = tkinter.Label(window)
+        self.label.pack()
         # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 10
         self.update()
-
         self.window.mainloop()
 
-    def update(self):
+    def update(self):        
         try:
-            frame = self.image_queue.get(timeout=0.2)  # Timeout to not block this method forever
-            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-            self.window.after(self.delay, self.update)
+            
+            self.imagen=PhotoImage(file="negro-blank.png")
+            self.label.configure(image=self.imagen)
+            self.label.image=self.imagen
+            time.sleep(0.3)
+            #self.window.after(self.delay,self.update)
         except Empty:
             pass  # try again next time
 
@@ -202,21 +197,21 @@ class App:
 def main():
     faceNet,maskNet=cargar_modelo()
     stream, width, height = setup_webcam_stream(0)
-    cola_ui = Queue()
+    resultado_queue = Queue()
+    #evento_ui = Event()
     cola_processing = Queue()
     #processed_queue = Queue(maxsize=1000)
     stop_event = Event()
-    window_name = "FPS Multi Threading"
     pausa_event= Event()
     try:
-        Thread(name="hilo1",target=video_stream_loop, args=[stream, cola_ui,cola_processing, stop_event,pausa_event]).start()
-        Thread(name="hilo2",target=processing_loop, args=[cola_processing , stop_event,pausa_event, faceNet, maskNet]).start()
-        App(tkinter.Tk(), window_name, cola_ui, (width, height))
+        Thread(name="hilo1",target=video_stream_loop, args=[stream, cola_processing, stop_event,pausa_event]).start()
+        Thread(name="hilo2",target=processing_loop, args=[cola_processing ,resultado_queue, stop_event,pausa_event, faceNet, maskNet]).start()
+        App(tkinter.Tk(), resultado_queue)
     finally:
         print("APRETANDO STOP")
         stop_event.set()
 
-    print(f"UI queue: {cola_ui.qsize()}")
+    print(f"UI queue: {resultado_queue.qsize()}")
     print(f"process queue: {cola_processing.qsize()}")
     
   #  print(f"Processed queue: {processed_queue.qsize()}")
